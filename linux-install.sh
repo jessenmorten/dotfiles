874dbb8776e5f2101c4e231393861525832830b7
Morten Jessen <mortenjessen5d@gmail.com>
2023-01-13 22:24:31 +0100

    add tmux config

--- linux-install.sh
+++ linux-install.sh
@@ -5,5 +5,12 @@ echo "Installing NeoVim configuration..."
 rm -rf ~/.config/nvim
 cp -r ./nvim ~/.config/nvim
 
+# Tmux
+echo "Installing Tmux configuration..."
+rm -rf ~/.tmux/plugins/tpm
+rm -rf ~/.tmux
+git clone https://github.com/tmux-plugins/tpm.git ~/.tmux/plugins/tpm
+cp ./tmux/.tmux.conf ~/.tmux.conf
+
 # Install complete
 echo "Dotfiles installed successfully!"
