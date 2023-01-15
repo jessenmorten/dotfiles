96e5fd84ac746764e3523b223c2231d631aedd76
Morten Jessen <mortenjessen5d@gmail.com>
2023-01-15 12:47:38 +0100

    check deps on windows

--- windows-install.sh
+++ windows-install.sh
@@ -1,4 +1,16 @@
-echo "Installing dotfiles..."
+# Exit script if any statement returns a non-true return value
+set -e
+
+# Ensure dependencies are installed
+echo "Checking dependencies..."
+git --version
+nvim --version
+cargo --version
+rustc --version
+rustup --version
+rg --version
+go version
+make --version
 
 # NeoVim
 echo "Installing NeoVim configuration..."
