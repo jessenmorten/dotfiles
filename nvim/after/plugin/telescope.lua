4b152091e6288a84cc489c488eb2326f87dae0a6
Morten Jessen <38260921+JessenMorten@users.noreply.github.com>
2023-07-12 14:26:43 +0200

    feat: search git commits

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -18,6 +18,7 @@ vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc =
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sb', require('telescope.builtin').git_branches, { desc = '[S]earch git [B]ranches' })
+vim.keymap.set('n', '<leader>sc', require('telescope.builtin').git_commits, { desc = '[S]earch git [C]ommits' })
 vim.keymap.set('n', '<leader>ss', require('telescope.builtin').git_status, { desc = '[S]earch git [S]tatus' })
 vim.keymap.set('n', '<C-p>', require("telescope.builtin").git_files, { desc = '[S]earch [G]it [F]iles' })
 vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
