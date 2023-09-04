81f2d85c96b0585286ae2ba9d64ca17b065d923a
Morten Jessen <mortenjessen5d@gmail.com>
2023-09-04 20:19:51 +0200

    feat: de-bloat

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,55 +99,6 @@ local function setup_plugins()
             end,
         },
         {
-            "akinsho/bufferline.nvim",
-            event = "BufReadPre",
-            config = true,
-            opts = {
-                options = {
-                    diagnostics = "nvim_lsp",
-                }
-            }
-        },
-        {
-            "folke/noice.nvim",
-            event = "VeryLazy",
-            dependencies = { "MunifTanjim/nui.nvim", lazy = true },
-            opts = {
-                lsp = {
-                    override = {
-                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                        ["vim.lsp.util.stylize_markdown"] = true,
-                        ["cmp.entry.get_documentation"] = true,
-                    },
-                    hover = {
-                        enabled = false,
-                    },
-                    signature = {
-                        enabled = false,
-                    }
-                },
-                routes = {
-                    {
-                        filter = {
-                            event = "msg_show",
-                            any = {
-                                { find = "%d+L, %d+B" },
-                                { find = "; after #%d+" },
-                                { find = "; before #%d+" },
-                            },
-                        },
-                        view = "mini",
-                    },
-                },
-                presets = {
-                    bottom_search = true,
-                    command_palette = true,
-                    long_message_to_split = true,
-                    inc_rename = true,
-                },
-            },
-        },
-        {
             "catppuccin/nvim",
             lazy = true,
             name = "catppuccin",
