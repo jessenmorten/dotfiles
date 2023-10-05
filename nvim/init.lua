1209477ca059f9eece0027adcea82b92dcfdb15f
Morten Jessen <mortenjessen5d@gmail.com>
2023-10-05 20:54:36 +0200

    feat: migrate indent-blankline 2 -> 3

--- nvim/init.lua
+++ nvim/init.lua
@@ -181,24 +181,11 @@ local function setup_plugins()
         {
             "lukas-reineke/indent-blankline.nvim",
             event = { "BufReadPost", "BufNewFile" },
+            main = "ibl",
             opts = {
-                -- char = "▏",
-                char = "│",
-                filetype_exclude = {
-                    "help",
-                    "alpha",
-                    "dashboard",
-                    "neo-tree",
-                    "Trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "lazyterm",
-                },
-                show_trailing_blankline_indent = false,
-                show_current_context = false,
-            },
+                indent = { char = "│" },
+                scope = { enabled = false },
+            }
         },
         {
             "echasnovski/mini.indentscope",
