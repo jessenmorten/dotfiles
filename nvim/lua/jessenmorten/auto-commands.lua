f0d57d516c89de4b02c808f688341137bf13c68f
Morten Jessen <mortenjessen5d@gmail.com>
2024-01-10 22:08:46 +0100

    remove lines numbers and signcolumn on TermOpen

--- nvim/lua/jessenmorten/auto-commands.lua
+++ nvim/lua/jessenmorten/auto-commands.lua
@@ -1,9 +1,18 @@
-local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
-
-vim.api.nvim_create_autocmd('TextYankPost', {
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
     callback = function()
         vim.highlight.on_yank()
     end,
     group = highlight_group,
-    pattern = '*',
+    pattern = "*",
+})
+
+local term_group = vim.api.nvim_create_augroup("TermOpen", { clear = true })
+vim.api.nvim_create_autocmd("TermOpen", {
+    callback = function()
+        vim.cmd("setlocal nonumber norelativenumber signcolumn=no")
+        vim.cmd("startinsert")
+    end,
+    group = term_group,
+    pattern = "*",
 })
