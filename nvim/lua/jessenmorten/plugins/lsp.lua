21c579754c4f125404b0812ad2aae62d72c87144
Morten Jessen <mortenjessen5d@gmail.com>
2024-04-04 22:24:33 +0200

    refactor: cleanup lsp config

--- nvim/lua/jessenmorten/plugins/lsp.lua
+++ nvim/lua/jessenmorten/plugins/lsp.lua
@@ -1,111 +1,174 @@
-local ensure_installed = {
-    "lua_ls",
-    "tsserver",
-    "gopls",
-    "csharp_ls",
+local servers = {
+    csharp_ls = {},
+    gopls = {},
+    rust_analyzer = {},
+    tsserver = {},
+    lua_ls = {
+        settings = {
+            Lua = {
+                completion = {
+                    callSnippet = "Replace",
+                },
+            },
+        },
+    },
 }
-
-local handlers = {
-    ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
-    ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-}
-
-local function set_sign_icons(opts)
-    opts = opts or {}
-
-    if vim.diagnostic.count then
-        local ds = vim.diagnostic.severity
-        local levels = {
-            [ds.ERROR] = "error",
-            [ds.WARN] = "warn",
-            [ds.INFO] = "info",
-            [ds.HINT] = "hint"
-        }
-
-        local text = {}
-
-        for i, l in pairs(levels) do
-            if type(opts[l]) == "string" then
-                text[i] = opts[l]
-            end
-        end
-
-        vim.diagnostic.config({ signs = { text = text } })
-        return
-    end
-
-    local sign = function(args)
-        if opts[args.name] == nil then
-            return
-        end
-
-        vim.fn.sign_define(args.hl, {
-            texthl = args.hl,
-            text = opts[args.name],
-            numhl = ""
-        })
-    end
-
-    sign({ name = "error", hl = "DiagnosticSignError" })
-    sign({ name = "warn", hl = "DiagnosticSignWarn" })
-    sign({ name = "hint", hl = "DiagnosticSignHint" })
-    sign({ name = "info", hl = "DiagnosticSignInfo" })
-end
-
-local on_attach = function(client, bufnr)
-    vim.notify("Attached " .. client.name, 2)
-    local tele = require("telescope.builtin")
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-
-    vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
-
-    vim.keymap.set("n", "gi", tele.lsp_implementations, opts)
-    vim.keymap.set("n", "<leader>gr", tele.lsp_references, opts)
-    vim.keymap.set("n", "<leader>ds", tele.lsp_document_symbols, opts)
-    vim.keymap.set("n", "<leader>ws", tele.lsp_dynamic_workspace_symbols, opts)
-    vim.keymap.set("n", "<leader>gt", tele.lsp_type_definitions, opts)
-
-    -- create ":Format" command
-    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-        vim.lsp.buf.format()
-        vim.api.nvim_command("w")
-    end, { desc = "Format current buffer with LSP" })
-end
 
 return {
     "neovim/nvim-lspconfig",
     dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-telescope/telescope-ui-select.nvim",
-        "j-hui/fidget.nvim",
+        -- Automatically install LSPs and related tools to stdpath for Neovim
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        {
+            "j-hui/fidget.nvim",
+            opts = {
+                notification = {
+                    override_vim_notify = true,
+                    view = {
+                        stack_upwards = false,
+                    },
+                    window = {
+                        winblend = 0,
+                        align = "top",
+                    },
+                },
+            },
+        },
+        { "folke/neodev.nvim", opts = {} },
     },
-    event = { "BufReadPost" },
     config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = ensure_installed,
-        })
-        local lsp_config = require("lspconfig")
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { remap = false })
 
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+            callback = function(event)
+                local tele = require("telescope.builtin")
+                local opts = { buffer = event.buf, remap = false }
+
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+
+                vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
+
+                vim.keymap.set("n", "gi", tele.lsp_implementations, opts)
+                vim.keymap.set("n", "<leader>gr", tele.lsp_references, opts)
+                vim.keymap.set("n", "<leader>ds", tele.lsp_document_symbols, opts)
+                vim.keymap.set("n", "<leader>ws", tele.lsp_dynamic_workspace_symbols, opts)
+                vim.keymap.set("n", "<leader>gt", tele.lsp_type_definitions, opts)
+
+                -- create ":Format" command
+                vim.api.nvim_buf_create_user_command(event.buf, "Format", function(_)
+                    vim.lsp.buf.format()
+                    vim.api.nvim_command("w")
+                end, { desc = "Format current buffer with LSP" })
+
+                local client = vim.lsp.get_client_by_id(event.data.client_id)
+                if client and client.server_capabilities.documentHighlightProvider then
+                    -- Highlight references of the word under cursor
+                    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.document_highlight,
+                    })
+
+                    -- Clear references when cursor moves
+                    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+                        buffer = event.buf,
+                        callback = vim.lsp.buf.clear_references,
+                    })
+                end
+
+                vim.notify(client.name .. " attached to buffer")
+            end,
+        })
+
+        -- Extend capabilities to include completion capabilities
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+
+        -- Ensure the servers and tools above are installed
+        require("mason").setup()
+        local ensure_installed = vim.tbl_keys(servers or {})
+        vim.list_extend(ensure_installed, {
+            "stylua", -- Used to format Lua code
+        })
+        require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+        -- Round virtual text and float borders
         vim.diagnostic.config({
             virtual_text = {
                 prefix = "●",
             },
             float = {
-                border = "rounded"
+                border = "rounded",
             },
         })
+
+        -- Rounded borders for hover and signature help
+        local handlers = {
+            ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
+            ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
+        }
+
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    -- This handles overriding only values explicitly passed
+                    -- by the server configuration above. Useful when disabling
+                    -- certain features of an LSP (for example, turning off formatting for tsserver)
+                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    server.handlers = vim.tbl_deep_extend("force", {}, handlers, server.handlers or {})
+                    require("lspconfig")[server_name].setup(server)
+                end,
+            },
+        })
+
+        local function set_sign_icons(opts)
+            opts = opts or {}
+
+            if vim.diagnostic.count then
+                local ds = vim.diagnostic.severity
+                local levels = {
+                    [ds.ERROR] = "error",
+                    [ds.WARN] = "warn",
+                    [ds.INFO] = "info",
+                    [ds.HINT] = "hint",
+                }
+
+                local text = {}
+
+                for i, l in pairs(levels) do
+                    if type(opts[l]) == "string" then
+                        text[i] = opts[l]
+                    end
+                end
+
+                vim.diagnostic.config({ signs = { text = text } })
+                return
+            end
+
+            local sign = function(args)
+                if opts[args.name] == nil then
+                    return
+                end
+
+                vim.fn.sign_define(args.hl, {
+                    texthl = args.hl,
+                    text = opts[args.name],
+                    numhl = "",
+                })
+            end
+
+            sign({ name = "error", hl = "DiagnosticSignError" })
+            sign({ name = "warn", hl = "DiagnosticSignWarn" })
+            sign({ name = "hint", hl = "DiagnosticSignHint" })
+            sign({ name = "info", hl = "DiagnosticSignInfo" })
+        end
 
         set_sign_icons({
             error = "",
@@ -113,13 +176,5 @@ return {
             hint = "",
             info = "",
         })
-
-        for _, server in ipairs(ensure_installed) do
-            lsp_config[server].setup({
-                capabilities = capabilities,
-                on_attach = on_attach,
-                handlers = handlers,
-            })
-        end
     end,
 }
--- nvim/lua/jessenmorten/plugins/lualine.lua
+++ nvim/lua/jessenmorten/plugins/lualine.lua
@@ -2,13 +2,15 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("lualine").setup {
+        require("lualine").setup({
             options = {
                 theme = "auto",
+                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
             },
             sections = {
                 lualine_a = { "mode" },
-                lualine_b = {},
+                lualine_b = { "filetype" },
                 lualine_c = {},
                 lualine_x = {},
                 lualine_y = {},
@@ -27,14 +29,14 @@ return {
                 lualine_b = {
                     {
                         "tabs",
-                        mode = 2,
-                    }
+                        mode = 0,
+                    },
                 },
                 lualine_c = {},
                 lualine_x = {},
                 lualine_y = { "branch" },
-                lualine_z = { "datetime" }
+                lualine_z = { "datetime" },
             },
-        }
-    end
+        })
+    end,
 }
